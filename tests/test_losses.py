Fix inf/nan in log-likelihood loss

tests/test_losses.py
@@ -3,7 +3,7 @@ import keras.backend as K  # noqa
 
 from deepquantiles.losses import _mdn_phi_tensor, _mdn_loss_tensor
 
-n = 10  # examples
+n = 1000  # examples
 m = 3  # mixture components
 
 
@@ -21,7 +21,11 @@ def test_mdn_loss_1D():
     sigma = K.variable(value=sigma_val)
 
     phi_tensor = _mdn_phi_tensor(y, mu, sigma)
-    assert phi_tensor.shape == (n, m)
-
     loss_tensor = _mdn_loss_tensor(y, w, mu, sigma)
+    assert phi_tensor.shape == (n, m)
     assert loss_tensor.shape == (n, )
+
+    phi = K.eval(phi_tensor)
+    loss = K.eval(loss_tensor)
+    assert np.isfinite(phi).all()
+    assert np.isfinite(loss).all()

tests/test_regressors.py
@@ -4,7 +4,6 @@ import numpy as np
 
 from deepquantiles import MultiQuantileRegressor, CDFRegressor, MixtureDensityRegressor
 
-
 n = 10
 n_features = 1
 
@@ -35,19 +34,26 @@ class TestQuantileRegressor:
     def test_predict(self, fitted_model, Xy):
         X, y = Xy
         y_hat = fitted_model.predict(X)
+        assert np.isfinite(y_hat).all()
         assert y_hat.shape[0] == X.shape[0]
         assert y_hat.shape[1] == len(fitted_model.quantiles)
 
     def test_sample(self, fitted_model, Xy):
         X, y = Xy
         num_samples = 7
-        y_hat = fitted_model.sample(X, num_samples=num_samples)
-        assert y_hat.shape == (X.shape[0], num_samples)
+        y_samples = fitted_model.sample(X, num_samples=num_samples)
+        assert np.isfinite(y_samples).all()
+        assert y_samples.shape == (X.shape[0], num_samples)
 
 
 class TestMDNRegressor:
 
     n_components = 3
+
+    @pytest.fixture(scope='class')
+    def unfitted_model(self, Xy):
+        model = MixtureDensityRegressor(n_components=self.n_components)
+        return model
 
     @pytest.fixture(scope='class')
     def fitted_model(self, Xy):
@@ -56,15 +62,35 @@ class TestMDNRegressor:
         model.fit(X, y, epochs=1, verbose=0)
         return model
 
-    def test_fit(self, fitted_model):
-        pass
-
-    def test_predict(self, fitted_model, Xy):
+    def test_predict(self, unfitted_model, Xy):
         X, y = Xy
-        w, mu, sigma = fitted_model.predict(X)
+        w, mu, sigma = unfitted_model.predict(X)
+        assert np.isfinite(w).all()
+        assert np.isfinite(mu).all()
+        assert np.isfinite(sigma).all()
         assert w.shape == (y.shape[0], self.n_components)
         assert mu.shape == (y.shape[0], self.n_components)
         assert sigma.shape == (y.shape[0], self.n_components)
+        assert np.allclose(w.sum(axis=1), 1)
+
+    def test_evaluate_loss(self, unfitted_model, Xy):
+        X, y = Xy
+        losses = unfitted_model.model['loss'].evaluate([X, y], y, verbose=0)
+        assert np.isfinite(losses).all()
+
+    def test_fit(self, fitted_model):
+        pass
+
+    def test_fit_predict(self, fitted_model, Xy):
+        X, y = Xy
+        w, mu, sigma = fitted_model.predict(X)
+        assert np.isfinite(w).all()
+        assert np.isfinite(mu).all()
+        assert np.isfinite(sigma).all()
+        assert w.shape == (y.shape[0], self.n_components)
+        assert mu.shape == (y.shape[0], self.n_components)
+        assert sigma.shape == (y.shape[0], self.n_components)
+        assert np.allclose(w.sum(axis=1), 1)
 
     def test_sample(self, fitted_model, Xy):
         pass