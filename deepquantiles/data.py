ec26880f3deb617fbb9f50f466797f994fbf8c04
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2019-02-16 00:44:55 +0000

    Add util functions

---
+++ deepquantiles/data.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def bishop_data(num=1000, sort=True):
+    y = np.linspace(0, 1, num=num)
+    x = y + 0.3 * np.sin(2 * np.pi * y) + 0.2 * np.random.rand(num) - 0.1
+    if sort:
+        order = x.argsort()
+        x, y = x[order], y[order]
+    return x.reshape(-1, 1), y
---
+++ deepquantiles/nb_utils.py
@@ -0,0 +1,12 @@
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
+
+def vis_model(model, show_shapes=False, show_layer_names=True, rankdir='TB'):
+    """Visualize model in a notebook."""
+    return SVG(
+        model_to_dot(
+            model,
+            show_shapes=show_shapes, show_layer_names=show_layer_names, rankdir=rankdir
+        ).create(prog='dot', format='svg')
+    )
